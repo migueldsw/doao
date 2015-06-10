f1116b1620be07052783e6fe66e23375bd572189
mdsw <miguel_dsw@hotmail.com>
2015-06-10 13:10:36 -0300

    add KFoldValidation_KNN total error

--- classifiers.py
+++ classifiers.py
@@ -2,7 +2,6 @@ import numpy as np
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
 
-
 def evaluateKNN(trainData, trainTarget, testData, testTarget):
 	kList = [1,3,5,7,10,20,30]#1,3,5,7,10,20,30
 	error = 100.0
@@ -16,9 +15,6 @@ def evaluateKNN(trainData, trainTarget, testData, testTarget):
 		if(error_temp < error):
 			error = error_temp
 			best_k = k
-
-
-	error = (error/len(testData))*100
 	#print "KNN error = %f" %error
 	#print "for k = %d" %best_k
 	return error, best_k
@@ -34,7 +30,7 @@ def random(X,Y):
 	return newX, newY
 
 def get10Fold_n(X,Y,n):
-	#get the n-th fold of X,Y as a test set, and all others folds as train set. of 10 folds
+	#get the n-th fold (of 10 folds) of X,Y as a test set, and all remaining folds as train set.
 	testX = getFold(X,n)
 	testY = getFold(Y,n)
 	trainX = []
@@ -49,19 +45,15 @@ def getFold(l,n):
 	#get the n-th fold (starting from 0, of 10 folds {0,...,9}) from the list l
 	return l[n*len(l)/10:(n+1)*len(l)/10]
 	 
-
 def KFoldValidation_KNN(X,Y):
-	error = 100.0
+	total_error = 0.0
 	for i in range(10):
 		a,b,c,d = get10Fold_n(X,Y,i)
-		erro, bestk = evaluateKNN(a,b,c,d )
-		print "fold %d:  error:  %f  , best k: %d" %(i+1,erro,bestk)
-
-
-#evaluateKNN(X[:-2], y[:-2],X[-2:], y[-2:])
-#evaluateKNN(getFold(X,0),getFold(y,0),getFold(X,1),getFold(y,1))
-#a,b,c,d = get10Fold_n(X,y,0)
-#evaluateKNN(a,b,c,d )
+		error, bestk = evaluateKNN(a,b,c,d )
+		print "fold %d:  error:  %f  , best k: %d" %(i+1,error,bestk)
+		total_error += error
+	total_error = (float(total_error)/len(Y))*100
+	print "total error: %f %%" %total_error
 
 print "-----Avaliando KNN para IRIS - KFOLD-cross validation (10 folds)"
 iris = datasets.load_iris()
