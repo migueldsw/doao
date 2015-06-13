02062315612496f3c9799ac3ba383dfa2df5a671
migueldsw <miguel_dsw@hotmail.com>
2015-06-13 10:17:46 -0300

    adding DT classifier

--- classifiers.py
+++ classifiers.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from collections import Counter as ctr
 
 def evaluateKNN(trainData, trainTarget, testData, testTarget):
@@ -21,6 +22,27 @@ def evaluateKNN(trainData, trainTarget, testData, testTarget):
 	#print "KNN error = %f" %error
 	#print "for k = %d" %best_k
 	return error, best_k, bestClassifier
+
+def evaluateDT(trainData, trainTarget, testData, testTarget):
+	mLeafList = [1,2,3,5] # Min. datapoints  in a LEAF node
+	mParentList = [5,10] #Min. datapoints  in a PARENT node
+	prune = True #prune
+	dtList = []#classifier list
+	error = 100000000.0
+	bestClassifier = []
+	for l in mLeafList:
+		for p in mParentList:
+			dt = DecisionTreeClassifier(random_state=0, min_samples_leaf=l,min_samples_split=p)
+			dt.fit(trainData,trainTarget)
+			dtList.append(dt)
+	for dt in dtList:
+		error_temp = 0.0
+		for i in range(len(testData)):
+			if (dt.predict(testData[i])[0] != testTarget[i]): error_temp += 1.0
+		if(error_temp < error):
+			error = error_temp
+			bestClassifier = dt
+	return error, bestClassifier
 
 def random(X,Y):
 	z = zip(X,Y)
@@ -46,16 +68,19 @@ def get10Fold_n(X,Y,n):
 def getFold(l,n):
 	#get the n-th fold (starting from 0, of 10 folds {0,...,9}) from the list l
 	return l[n*len(l)/10:(n+1)*len(l)/10]
-	 
-def KFoldValidation_KNN(X,Y):
+	
+def KFoldValidation(X,Y,classifierName):
+##classifiers names:  ANN, DT, KNN, LDA, LR, SVM
 	total_error = 0.0
 	for i in range(10):
 		a,b,c,d = get10Fold_n(X,Y,i)
-		error, bestk, bestClassifier = evaluateKNN(a,b,c,d )
-		print "fold %d:  error:  %f  , best k: %d" %(i+1,error,bestk)
+		if classifierName == "KNN":
+			error, bestk, bestClassifier = evaluateKNN(a,b,c,d )
+		elif classifierName == "DT":
+			error, bestClassifier = evaluateDT(a,b,c,d )
 		total_error += error
 	total_error = (float(total_error)/len(Y))*100
-	print "total error: %f %%" %total_error
+	#print "total error: %f %%" %total_error
 	return total_error, bestClassifier
 
 #TODO# def KFoldValidatin_CLASSIFIER2,3...(X,Y)
@@ -80,15 +105,33 @@ def OAODataSet(X,Y,OAOPair):#getOAODataSet
 			newY.append(Y[i])
 	return newX, newY
 
+def OAO(X,Y,classifierName):
+	pairs = OAOPairList(Y)
+	bestError = 100
+	bestPair = []
+	classifierSet = []
+	for p in pairs:
+		#print "for classes: %d and %d:" %(p[0],p[1])
+		nx, ny = OAODataSet(X,Y,p)
+		error, best_knn = KFoldValidation(nx,ny,classifierName)
+		#TODO# error2, bestclassifier2 = KFoldValidatin_CLASSIFIER2(nx,ny)
+		#(find the best error)
+		classifierSet.append(best_knn) #TODO# change to append the best classifier
+		if (bestError > error):
+			bestError = error
+			bestPair = p
+	print "\nBEST ERROR: %f %% | for class %d vs %d" %(bestError,bestPair[0],bestPair[1])
+	return classifierSet
+
 def DOAO(X,Y):
 	pairs = OAOPairList(Y)
 	bestError = 100
 	bestPair = []
 	classifierSet = []
 	for p in pairs:
-		print "for classes: %d and %d:" %(p[0],p[1])
+		#print "for classes: %d and %d:" %(p[0],p[1])
 		nx, ny = OAODataSet(X,Y,p)
-		error, best_knn = KFoldValidation_KNN(nx,ny)
+		error, best_knn = KFoldValidation(nx,ny,"KNN")
 		#TODO# error2, bestclassifier2 = KFoldValidatin_CLASSIFIER2(nx,ny)
 		#(find the best error)
 		classifierSet.append(best_knn) #TODO# change to append the best classifier
@@ -129,14 +172,29 @@ def OARValidation(X,Y,classifier):
 				total_error += 1
 	return (float(total_error)/len(Y))*100
 
-print "-----Avaliando KNN para IRIS - KFOLD-cross validation (10 folds)OAR"
+def OAO_OAR_KNN(X,y):
+	e, knn = KFoldValidation(X,y,"KNN")
+	print "-----Avaliando KNN para IRIS - KFOLD-cross validation (10 folds) OAO_OAR"
+	oao = OAO(X,y,"KNN")
+	print "-----KNN-OAO Validation Error:-----"
+	print OAOValidation(X,y,oao)
+	print "-----KNN-OAR Validation Error:-----"
+	print OARValidation(X,y,knn)
+def OAO_OAR_DT(X,y):
+	e, knn = KFoldValidation(X,y,"DT")
+	print "-----Avaliando DT para IRIS - KFOLD-cross validation (10 folds) OAO_OAR"
+	oao = OAO(X,y,"DT")
+	print "-----DT-OAO Validation Error:-----"
+	print OAOValidation(X,y,oao)
+	print "-----DT-OAR Validation Error:-----"
+	print OARValidation(X,y,knn)
+
+
+##EXECUTE#############
+print "----MAIN----"
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
 X,y = random(X,y)
-e, knn = KFoldValidation_KNN(X,y)
-print "-----Avaliando KNN para IRIS - KFOLD-cross validation (10 folds) OAO"
-doao = DOAO(X,y)
-print "-----KNN-OAO Validation Error:-----"
-print OAOValidation(X,y,doao)
-print "-----KNN-OAR Validation Error:-----"
-print OARValidation(X,y,knn)
+OAO_OAR_KNN(X,y)
+OAO_OAR_DT(X,y)
+
